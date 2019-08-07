f965bb193f281e9edef83e2d13310bb22a07c082
Michael <mbalint1@gmail.com>
2019-08-07 15:37:34 -0500

    initial

---
+++ Mobile.AutoBuyer/Mobile.AutoBuyer/Mobile.AutoBuyer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mobile.AutoBuyer.Models;
+
+namespace Mobile.AutoBuyer.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
